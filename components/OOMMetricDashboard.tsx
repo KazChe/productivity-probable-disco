b90fdab02331824377fb7065b064e2dbcc01e600
kazche <kaz.che@gmail.com>
2024-09-19 11:47:47 -0700

    resolves issue #12

--- components/OOMMetricDashboard.tsx
+++ components/OOMMetricDashboard.tsx
@@ -6,7 +6,6 @@ import { Bar, BarChart, CartesianGrid, LabelList, XAxis, YAxis } from "recharts"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -59,104 +58,107 @@ const chartConfig = {
 export default function OOMMetricDashboard() {
   return (
     <div className="flex gap-4">
-      <Card className="bg-gray-800 text-white-100 w-1/2"> 
-        <CardHeader>
-          <CardTitle>Metaspace OOMs - Daily Metrics</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <ChartContainer config={chartConfig}>
-            <BarChart
-              accessibilityLayer
-              data={chartData}
-              layout="vertical"
-              margin={{
-                right: 50,
-              }}
-            >
-              <CartesianGrid horizontal={false} />
-              <YAxis
-                dataKey="day"
-                type="category"
-                tickLine={false}
-                tickMargin={10}
-                axisLine={false}
-                hide
-              />
-              <XAxis dataKey="ooms" type="number" hide />
-              <ChartTooltip
-                cursor={false}
-                content={<ChartTooltipContent indicator="dot" />}
-                labelFormatter={(label) => (
-                  <span style={{ fontStyle: "bold", fontSize: "14px", color: "black"}}>{label}</span>
-                )}
-                formatter={(value) => (
-                  <span style={{ fontStyle: "bold",fontSize: "14px", color: "black"}}>{`${value} errors`}</span>
-                )}
-                wrapperStyle={{
-                  backgroundColor: 'rgba(255, 255, 255, 0.8)',
-                  borderRadius: '5px',
-                }}
-              />
-              <Bar
-                dataKey="ooms"
+      <div className="w-1/2">
+        <Card className="bg-gray-800 text-white-100">
+          <CardHeader>
+            <CardTitle>Metaspace OOMs - Daily Metrics</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer config={chartConfig}>
+              <BarChart
+                accessibilityLayer
+                data={chartData}
                 layout="vertical"
-                fill="var(--color-ooms)"
-                radius={5}
-                width={100}
+                margin={{
+                  right: 50,
+                }}
               >
-                <LabelList
+                <CartesianGrid horizontal={false} />
+                <YAxis
                   dataKey="day"
-                  position="insideLeft"
-                  offset={8}
-                  className="fill-[--color-label]"
-                  fontSize={14}
-                  style={{ fill: "white" }}
+                  type="category"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  hide
                 />
-                <LabelList
+                <XAxis dataKey="ooms" type="number" hide />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent indicator="dot" />}
+                  labelFormatter={(label) => (
+                    <span style={{ fontStyle: "bold", fontSize: "14px", color: "black"}}>{label}</span>
+                  )}
+                  formatter={(value) => (
+                    <span style={{ fontStyle: "bold",fontSize: "14px", color: "black"}}>{`${value} errors`}</span>
+                  )}
+                  wrapperStyle={{
+                    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                    borderRadius: '5px',
+                  }}
+                />
+                <Bar
                   dataKey="ooms"
-                  position="right"
-                  offset={8}
-                  style={{ fill: "white" }}
-                  className="fill-foreground "
-                  fontSize={15}
-                />
-              </Bar>
-            </BarChart>
-          </ChartContainer>
-        </CardContent>
-        <CardFooter className="flex-col items-start gap-2 text-sm">
-          
-          <div className="flex gap-2 font-medium leading-none">
-            Trending up/down by xx% this week <TrendingUp className="h-4 w-4" />
-          </div>
-          <div className="leading-none text-muted-foreground">
-            Showing total OOMs for each day of the week
-          </div>
-        </CardFooter>
-      </Card>
-      <Card className="bg-gray-800 text-white-100 w-1/2">
-        <CardHeader><CardTitle>Metaspace OOMs By Instances</CardTitle></CardHeader>
-        <CardContent className="flex flex-col gap-2">
-          <Accordion type="single" collapsible className="w-full">
-            {chartData.map((day) => (
-              <AccordionItem key={day.day} value={day.day}>
-                <AccordionTrigger className="flex justify-between">
-                  <span>{day.day}:</span>
-                  <span>{day.ooms} OOMs</span>
-                </AccordionTrigger>
-                <AccordionContent>
-                  {Object.entries(day.instances).map(([instance, count], index, array) => (
-                    <React.Fragment key={instance}>
-                      {count} {instance}
-                      {index < array.length - 1 ? ', ' : ''}
-                    </React.Fragment>
-                  ))}
-                </AccordionContent>
-              </AccordionItem>
-            ))}
-          </Accordion>
-        </CardContent>
-      </Card>
+                  layout="vertical"
+                  fill="var(--color-ooms)"
+                  radius={5}
+                  width={100}
+                >
+                  <LabelList
+                    dataKey="day"
+                    position="insideLeft"
+                    offset={8}
+                    className="fill-[--color-label]"
+                    fontSize={14}
+                    style={{ fill: "white" }}
+                  />
+                  <LabelList
+                    dataKey="ooms"
+                    position="right"
+                    offset={8}
+                    style={{ fill: "white" }}
+                    className="fill-foreground "
+                    fontSize={15}
+                  />
+                </Bar>
+              </BarChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter className="flex-col items-start gap-2 text-sm">
+            <div className="flex gap-2 font-medium leading-none">
+              Trending up/down by xx% this week <TrendingUp className="h-4 w-4" />
+            </div>
+            <div className="leading-none text-muted-foreground">
+              Showing total OOMs for each day of the week
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="w-1/2">
+        <Card className="bg-gray-800 text-white-100">
+          <CardHeader><CardTitle>Metaspace OOMs By Instances</CardTitle></CardHeader>
+          <CardContent className="flex flex-col gap-2">
+            <Accordion type="single" collapsible className="w-full">
+              {chartData.map((day) => (
+                <AccordionItem key={day.day} value={day.day}>
+                  <AccordionTrigger className="flex justify-between">
+                    <span>{day.day}:</span>
+                    <span>{day.ooms} OOMs</span>
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    {Object.entries(day.instances).map(([instance, count], index, array) => (
+                      <React.Fragment key={instance}>
+                        {count} {instance}
+                        {index < array.length - 1 ? ', ' : ''}
+                      </React.Fragment>
+                    ))}
+                  </AccordionContent>
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   )
 }
