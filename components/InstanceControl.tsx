9cf081df00f8b963675a5e813eb3bc7f740fe884
kazche <kaz.che@gmail.com>
2024-09-21 09:08:40 -0700

    css modifications for aura form

--- components/InstanceControl.tsx
+++ components/InstanceControl.tsx
@@ -159,7 +159,7 @@ export default function InstanceControl() {
 
   return (
     <div className="max-w-3xl mx-auto p-6 bg-card rounded-lg bg-gray-800 text-white-100">
-      <h1 className="text-2xl font-bold text-center">Instance Control</h1>
+      <h1 className="text-2xl font-bold text-center mb-4">Neo4j Aura Instance Control</h1>
       {alert && (
         <Alert variant={alert.variant} className="mb-4">
           <AlertTitle>{alert.title}</AlertTitle>
