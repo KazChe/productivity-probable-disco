722010b086c73e7fe8f421cc64741815c44f165b
kazche <kaz.che@gmail.com>
2024-09-25 22:43:20 -0700

    minor css tweak to widen the Aura Instance Control page

--- components/InstanceControl.tsx
+++ components/InstanceControl.tsx
@@ -134,7 +134,7 @@ export default function InstanceControl() {
   };
 
   return (
-    <div className="max-w-3xl mx-auto p-6 bg-card rounded-lg bg-gray-800 text-white-100">
+    <div className="max-w-100 mx-auto p-6 bg-card rounded-lg bg-gray-800 text-white-100">
       <h1 className="text-2xl font-bold text-center mb-4">
         Neo4j Aura Instance Control
       </h1>
@@ -172,6 +172,7 @@ export default function InstanceControl() {
                       key={instance.id}
                       className="flex items-center space-x-2 border-b border-gray-700 pb-2"
                     >
+                        
                       <Checkbox
                         className="border-white-100"
                         id={instance.id}
@@ -230,7 +231,7 @@ export default function InstanceControl() {
             </Button>
           </form>
         </div>
-        <Card className="bg-gray-800 text-white-100">
+        <Card className="bg-gray-800 text-white-100 ">
           <CardHeader>
             <CardTitle>Response</CardTitle>
           </CardHeader>
