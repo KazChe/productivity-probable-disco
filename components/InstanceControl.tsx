0b42469a145566999c6fadab0c2c86b7464c64e6
kazche <kaz.che@gmail.com>
2024-10-01 23:27:23 -0700

    update project structure

--- components/InstanceControl.tsx
+++ components/InstanceControl.tsx
@@ -34,8 +34,6 @@ const getAlertStyle = (variant: AlertType["variant"]) => {
   switch (variant) {
     case "success":
       return "bg-green-100 border-green-400 text-green-700";
-    case "destructive":
-      return "bg-red-100 border-red-400 text-red-700";
     case "warning":
       return "bg-yellow-100 border-yellow-400 text-yellow-700";
     default:
